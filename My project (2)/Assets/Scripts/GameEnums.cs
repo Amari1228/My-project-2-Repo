d748a401d4ce9860878e92e148f4ba45d5967d48
Amari1228 <amari.anderson@hgs.hiddengeniusproject.org>
2022-07-12 12:12:46 -0700

    Damage

---
+++ My project (2)/Assets/Scripts/GameEnums.cs
@@ -0,0 +1,15 @@
+YOUR GameEnums are based on whatever ability types & projectiles you have implemented within your game 
+ 
+public enum MovementPattern
+{
+   NormalForward,
+   BoomerangForward,
+   BoomerangSideways,
+   ZigZagForward,   
+}
+public enum UnitTypes
+{
+   Player,
+   Enemy,
+   Obstacle,
+}
